92bc310bc0c99521582a251954dd68f9fe41aee4
mlombardo27 <mlombardo2797@gmail.com>
2022-02-21 14:56:37 +0000

    made minor changes to functions to make the graphs for my thesis on total bdnf data without well 4

--- make_volcano_plot.R
+++ make_volcano_plot.R
@@ -13,18 +13,17 @@ make_volcano_plot <- function(res, gene_names = "") {
   
   my_plot <- ggplot(res, aes(x = log2FoldChange,
                              y= -log10(padj),
-                             fill = graph_color)) +
+                             fill = graph_color, 
+                             color = graph_color,)) +
     geom_point(aes(alpha = graph_alpha), pch = 21) +
-    theme(legend.position = 'top') +
     ggpubr::theme_pubr() +
-    scale_fill_manual(values = c("yellow", "gray", "purple")) +
     ylab(expression(paste("-Lo", g[10], "P-value"))) +
     xlab(expression(paste("Lo", g[2], "Fold Change"))) +
     theme(text = element_text(size = 24)) +
     theme(legend.text = element_text(size = 22)) +
     geom_hline(yintercept = -log10(0.1)) +
     geom_vline(xintercept = c(0), linetype = "dotted") +
-    guides(alpha = FALSE, size = FALSE, fill = FALSE)
+    guides(alpha = FALSE, size = FALSE)
   
   if(gene_names != ""){
     my_plot <- my_plot + geom_text(aes(label = plot_name))
@@ -60,14 +59,15 @@ label_significant <- function(res, log2FoldCut = 3, log10padj = 20){
   
   with_name <-first_plot$data %>% 
     as.data.frame() %>% 
-    rownames_to_column("ensgene") %>% 
-    left_join(annotables::grcm38)  %>% 
     filter(-log10(padj) > log10padj) %>% 
     filter(abs(log2FoldChange) > log2FoldCut)
   
-  print(dim(with_name))
   named_plot <- first_plot +
-    geom_text(data = with_name, aes(label = symbol))
+    geom_text_repel(data = with_name, aes(x = log2FoldChange,
+                                          y= -log10(padj),
+                                          label = gene_name),
+                    show_guide = FALSE)
   
   return(named_plot)
 }
+
