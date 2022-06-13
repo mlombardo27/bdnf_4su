f4b76d8b8a1f70830cf230f754c92e12b7e37494
mlombardo27 <mlombardo2797@gmail.com>
2022-06-13 16:57:15 +0100

    looking at genes at 2hr with downregulated total rna and higher fraction of new rna; creating plots of total rna expression for CHX inhibition genes; fixing titles; looking at orf loss

--- results_narrative_graphs_to_make.R
+++ results_narrative_graphs_to_make.R
@@ -19,6 +19,21 @@ go_genes_i_want <- smaller_new_ratio_bayesian_p_de %>%
   slice_min(mean_diff, n = 16) %>% 
   pull(gene_name)
 
+go_genes_2hr_downreg_total_higher_new_fp <- file.path('downregulated_twohours_plot_these_genes.csv')
+go_genes_2hr_downreg_total_higher_new <- read_csv(go_genes_2hr_downreg_total_higher_new_fp)
+
+go_genes_2hr_downreg_total_higher_new <- go_genes_2hr_downreg_total_higher_new %>% 
+  pull(geneID) %>% 
+  unique()
+
+go_genes_i_want2 <- smaller_new_ratio_bayesian_p_de %>% 
+  filter(gene_name %in% go_genes_2hr_downreg_total_higher_new)  %>% 
+  filter(time == 2) %>% 
+  slice_min(mean_diff, n = 16) %>% 
+  pull(gene_name)
+
+chx_test_genes <- c('DCTN2','DYNC1I2','HMGB2','MDK')
+
 estimate_list_full_fp <- file.path('data','estimate_list_full.csv')
 estimate_list_full <- read_csv(estimate_list_full_fp)
 
@@ -35,7 +50,33 @@ test <-estimate_list_full %>%
   ungroup()  %>% 
   mutate(max_y = max_y - 0.005)
 
-test %>% 
+test2 <-estimate_list_full %>% 
+  filter(symbol %in% chx_test_genes) %>% 
+  left_join(smaller_new_ratio_bayesian_p_de, by = c('symbol' = 'gene_name', 'time')) %>% 
+  unique() %>% 
+  filter(n_samp_passing_bdnf == 3 & n_samp_passing_control == 3) %>% 
+  mutate(condition = fct_relevel(condition,'control')) %>% 
+  mutate(sig_label = ifelse(new_rna_sig %in% c("unclear","bdnf_equals_control"),"", "*")) %>% 
+  mutate(sig_label = ifelse(is.na(new_rna_sig),"", sig_label)) %>% 
+  group_by(symbol,time) %>% 
+  mutate(max_y = max(map)) %>% 
+  ungroup()  %>% 
+  mutate(max_y = max_y - 0.005)
+
+test3 <-estimate_list_full %>% 
+  filter(symbol %in% go_genes_i_want2) %>%  
+  left_join(smaller_new_ratio_bayesian_p_de, by = c('symbol' = 'gene_name', 'time')) %>% 
+  unique() %>% 
+  filter(n_samp_passing_bdnf == 3 & n_samp_passing_control == 3) %>% 
+  mutate(condition = fct_relevel(condition,'control')) %>% 
+  mutate(sig_label = ifelse(new_rna_sig %in% c("unclear","bdnf_equals_control"),"", "*")) %>% 
+  mutate(sig_label = ifelse(is.na(new_rna_sig),"", sig_label)) %>% 
+  group_by(symbol,time) %>% 
+  mutate(max_y = max(map)) %>% 
+  ungroup()  %>% 
+  mutate(max_y = max_y - 0.005)
+
+test3 %>% 
   ggplot(aes(x = as.factor(time), y = map, fill = condition))+
   geom_boxplot() +
   geom_point(size = 1.5,pch = 21,position = position_dodge(width = 0.9)) +
@@ -53,16 +94,17 @@ test %>%
   facet_wrap(~symbol,scales = 'free_y') +
   geom_text(
     inherit.aes = FALSE, (aes(x = as.character(time), y = max_y,label = sig_label)),
-    size = 7)
+    size = 7)+
+  ggtitle('Change in the New Fraction of RNA \nBDNF vs Control 2hr \n downregulated total with higher fraction new RNA with BDNF')
 
 new_ratio_bayesian_p_de %>% 
-  filter(gene_name %in% go_genes_i_want) %>% 
+  filter(gene_name %in% go_genes_i_want2) %>% 
   mutate(sig_change = !(padj > 0.1 | is.na(padj))) %>% 
   ggplot(aes(x = time, y= log2FoldChange, fill = sig_change)) +
   geom_hline(yintercept = 0,size = 1)+
   geom_path(aes(x = time, y = log2FoldChange, group = gene_name))+
   geom_point(size = 3,pch = 21)+
-  ggtitle('Change in the New Fraction of RNA \nBDNF vs Control') +
+  ggtitle('Change in the Total RNA abundance \nBDNF vs Control 2hr') +
   scale_fill_manual(values = c("plum3","turquoise2")) +
   facet_wrap(~gene_name, scales = "free_y")+
   labs(x = "Treatment Time")+
